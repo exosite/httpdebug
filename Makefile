0f1fee845d3dabe8d3aa5f2768ee934597bda5b3
Alex Wauck <alexwauck@exosite.com>
2017-08-08 15:35:51 -0500

    Added Exosite proxy protocol v2 support

--- Makefile
+++ Makefile
@@ -8,13 +8,13 @@ windows: build/win-amd64/httpdebug.exe
 
 # Linux Build
 build/linux-amd64/httpdebug: main.go
-	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $@ github.com/waucka/httpdebug
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $@ github.com/exosite/httpdebug
 # OS X Build
 build/osx-amd64/httpdebug: main.go
-	GOOS=darwin GOARCH=amd64 go build -o $@ github.com/waucka/httpdebug
+	GOOS=darwin GOARCH=amd64 go build -o $@ github.com/exosite/httpdebug
 # Windows Build
 build/win-amd64/httpdebug.exe: main.go
-	GOOS=windows GOARCH=amd64 go build -o $@ github.com/waucka/httpdebug
+	GOOS=windows GOARCH=amd64 go build -o $@ github.com/exosite/httpdebug
 
 clean:
 	rm -f build/linux-amd64/httpdebug
--- main.go
+++ main.go
@@ -3,10 +3,90 @@ package main
 import (
 	"io"
 	"log"
+	"fmt"
+	"net"
 	"net/http"
 	"net/http/httputil"
+	"bytes"
 	"os"
+
+	proxyproto "github.com/exosite/proxyprotov2"
 )
+
+type ProxyListener struct {
+	listener net.Listener
+}
+
+func NewProxyListener(listenAddr string) (*ProxyListener, error) {
+	ln, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ProxyListener{
+		listener: ln,
+	}, nil
+}
+
+func (self *ProxyListener) Accept() (net.Conn, error) {
+	conn, err := self.listener.Accept()
+	if err != nil {
+		return conn, err
+	}
+	connInfo := fmt.Sprintf("%s->%s", conn.RemoteAddr().String(), conn.LocalAddr().String())
+
+	proxyInfo, bytesToWrite, err := proxyproto.HandleProxy(conn)
+	if err != nil {
+		if err == io.EOF {
+			// EOF?  Just return.  Screw it.
+			return conn, nil
+		}
+		log.Printf("[%s] Failed to handle proxy protocol: %s", connInfo, err.Error())
+		return conn, nil
+	}
+	if bytesToWrite != nil && len(bytesToWrite) > 0 {
+		return conn, fmt.Errorf("Read too much!")
+	}
+	if proxyInfo != nil {
+		for _, tlv := range proxyInfo.TLVs {
+			// log.Printf("[%s] TLV 0x%x: %#v", connInfo, tlv.Type, tlv.Value)
+			tlsInfo, isTls := tlv.(*proxyproto.TlsTLV)
+			if isTls {
+				log.Printf("[%s] TLS Version: %s", connInfo, tlsInfo.Version())
+				log.Printf("[%s] CN: %s", connInfo, tlsInfo.CN())
+				log.Printf("[%s] SNI: %s", connInfo, tlsInfo.SNI())
+				if tlsInfo.Certs != nil {
+					certs, err := tlsInfo.Certs()
+					if err != nil {
+						log.Printf("[%s] Failed to parse certificates: %s", err.Error())
+					} else {
+						for i, cert := range certs {
+							log.Printf("[%s] Certificate %d: %s", connInfo, i, cert.Subject.CommonName)
+						}
+					}
+				}
+				fp := tlsInfo.Fingerprint()
+				if fp != nil {
+					var shabuf bytes.Buffer
+					for _, part := range fp {
+						shabuf.WriteString(fmt.Sprintf("%x", part))
+					}
+					log.Printf("[%s] Cert SHA1: %s", connInfo, shabuf.String())
+				}
+			}
+		}
+	}
+
+	return conn, nil
+}
+
+func (self *ProxyListener) Close() error {
+	return self.listener.Close()
+}
+
+func (self *ProxyListener) Addr() net.Addr {
+	return self.listener.Addr()
+}
 
 type HandleAll struct {
 	Verbose bool
@@ -33,5 +113,15 @@ func main() {
 	hAll := HandleAll{
 		Verbose: os.Getenv("VERBOSE") == "y",
 	}
-	log.Fatal(http.ListenAndServe(":8080", &hAll))
+	var err error
+	var ln net.Listener
+	if os.Getenv("USE_PROXY_PROTO") == "y" {
+		ln, err = NewProxyListener(":8080")
+	} else {
+		ln, err = net.Listen("tcp", ":8080")
+	}
+	if err != nil {
+		log.Fatalf("Error setting up listener: %s", err.Error())
+	}
+	log.Fatal(http.Serve(ln, &hAll))
 }
